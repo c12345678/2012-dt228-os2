dd86287f2b7ab075975d83cbc142160129079fcb
Brian Gillespie <brian.x.gillespie@dit.ie>
2012-10-30 00:03:20 +0000

    Fix bug in test runner script

--- assignment1/tests/test_runner.rb
+++ assignment1/tests/test_runner.rb
@@ -29,18 +29,22 @@ def relative_path_to(file)
   '../' * current_depth + file
 end
 
+def path_for(cmd)
+  %x{which "#{cmd}"}.chomp
+end
+
 
 TIMEOUT=2
 
 @tests = [
-  {tc: 'Run a command with no arguments', depends: :EXTERNAL, cmd: %x{which uname}, expected: %x{uname}, explanation: "Unexpected output from running the a command"},
-  {tc: 'Check if fork() is implemented', depends: :EXTERNAL, cmd: %x{which ps}, expected: 'lsh', explanation: "Command not executed in fork()"},
-  {tc: 'Run a command with a single argument', depends: :EXTERNAL, cmd: %x{which uname} + ' -n', expected: %x{uname -n}, explanation: "No support for passing command line arguments (1 arg)"},
-  {tc: 'Run a command with three arguments', depends: :EXTERNAL, cmd: %x{which expr} + ' 1 + 1', expected: '^2', explanation: "No support for passing command line arguments (3 args)"},
-  {tc: 'Run a commmand with multiple arguments', depends: :EXTERNAL, cmd: %x{which expr} + ' 5 * 6 + 7 / 2 - 8 * 3', expected: '^9', explanation: "No support for passing command line arguments (11 args)"},
-  {tc: 'Check if PATH lookup is implemented for a command that exists', depends: :EXTERNAL, cmd: %x{which uname}, expected: 'Linux', explanation: "Command in PATH but not executed "},
+  {tc: 'Run a command with no arguments', depends: :EXTERNAL, cmd: path_for('uname'), expected: %x{uname}.chomp, explanation: "Unexpected output from running the a command"},
+  {tc: 'Check if fork() is implemented', depends: :EXTERNAL, cmd: path_for('ps'), expected: 'lsh', explanation: "Command not executed in fork()"},
+  {tc: 'Run a command with a single argument', depends: :EXTERNAL, cmd: path_for('uname') + ' -n', expected: %x{uname -n}.chomp, explanation: "No support for passing command line arguments (1 arg)"},
+  {tc: 'Run a command with three arguments', depends: :EXTERNAL, cmd: path_for('expr') + ' 1 + 1', expected: '^2', explanation: "No support for passing command line arguments (3 args)"},
+  {tc: 'Run a commmand with multiple arguments', depends: :EXTERNAL, cmd: path_for('expr') + ' 5 * 6 + 7 / 2 - 8 * 3', expected: '^9', explanation: "No support for passing command line arguments (11 args)"},
+  {tc: 'Check if PATH lookup is implemented for a command that exists', depends: :EXTERNAL, cmd: path_for('uname'), expected: %x{uname}.chomp, explanation: "Command in PATH but not executed "},
   {tc: 'Check how PATH lookup is implemented for a command that does not exist', depends: :EXTERNAL, cmd: 'xyz', expected: 'No such file or directory', explanation: "Command not in PATH but expected error not thrown"},
-  {tc: 'Check if relative path commands will run', depends: :EXTERNAL, cmd: relative_path_to(%x{which uname}), expected: 'Linux', explanation: "Relative path command not executed "},
+  {tc: 'Check if relative path commands will run', depends: :EXTERNAL, cmd: relative_path_to(path_for('uname')), expected: %x{uname}.chomp, explanation: "Relative path command not executed "},
   {tc: 'Check if environment variables are implemented', depends: :USERVARS, cmd: 'PS1="% "', expected: '% ', explanation: "Failed to set a new command prompt into PS1"}
 ]
 
@@ -61,7 +65,7 @@ begin
   valid_tests.each_with_index do |test, i|
     @run = i + 1
     PTY.spawn(ARGV[0]) do |lsh_out, lsh_in, pid|
-      lsh_out.expect(/^#{@prompt}$/, TIMEOUT) do |r|
+      lsh_out.expect(/#{@prompt}/, TIMEOUT) do |r|
         fail_and_exit "Doesn't seem to have run the shell" if r.nil?
         msg "unit-test-#{i}: " + test[:tc] + "\t(" + test[:cmd] + ')'
         lsh_in.print test[:cmd] + "\r"
