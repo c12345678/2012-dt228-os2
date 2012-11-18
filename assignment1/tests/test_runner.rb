66ec1037d2f020606060dace71b0f10aaa40c16d
Brian Gillespie <brian.x.gillespie@dit.ie>
2012-11-18 13:41:14 +0000

    Added grading extras to unit tests

--- assignment1/tests/test_runner.rb
+++ assignment1/tests/test_runner.rb
@@ -3,6 +3,7 @@
 
 require 'pty'
 require 'expect'
+require 'fileutils'
 
 class Runnable
   def redirectto
@@ -12,21 +13,33 @@ class Runnable
       "Not found"
     end
   end
+
+  def self.cleanup
+    begin
+      FileUtils.rm "outfile.txt"
+    rescue => e
+    end
+  end
 end
 
 def msg(*args)
-  $stderr.print *args
-  $stderr.print "\n"
+  unless @quiet
+    $stderr.print *args
+    $stderr.print "\n"
+  end
 end
 
 def fail(*args)
-  $stderr.print "\n"
-  $stderr.print "Fail: "
+  unless @quiet
+    $stderr.print "\n"
+    $stderr.print "Fail: "
+  end
   msg *args
   @fails += 1
 end
 
 def fail_and_exit(*args)
+  @quiet = false
   fail(*args)
   exit 1
 end
@@ -48,30 +61,33 @@ TIMEOUT=2
 @passes=0
 @fails=0
 @run=0
+@grade = 0.0
 @prompt=%x(make --silent showprompt 2>/dev/null).chomp
 @prompt="lsh>> " if @prompt == ""
+@grading = ARGV.include? "--grade"
+@quiet = @grading
 
 
 @tests = [
-  {tc: 'Run a command with no arguments', depends: :EXTERNAL, cmd: path_for('uname'), expected: %x{uname}.chomp, explanation: "Unexpected output from running the a command"},
-  {tc: 'Check if fork() is implemented', depends: :EXTERNAL, cmd: path_for('ps'), expected: 'lsh', explanation: "Command not executed in fork()"},
-  {tc: 'Run a command with a single argument', depends: :EXTERNAL, cmd: path_for('uname') + ' -n', expected: %x{uname -n}.chomp, explanation: "No support for passing command line arguments (1 arg)"},
-  {tc: 'Run a command with three arguments', depends: :EXTERNAL, cmd: path_for('expr') + ' 1 + 1', expected: '^2', explanation: "No support for passing command line arguments (3 args)"},
-  {tc: 'Run a commmand with multiple arguments', depends: :EXTERNAL, cmd: path_for('expr') + ' 5 * 6 + 7 / 2 - 8 * 3', expected: '^9', explanation: "No support for passing command line arguments (11 args)"},
-  {tc: 'Check if PATH lookup is implemented for a command that exists', depends: :EXTERNAL, cmd: path_for('uname'), expected: %x{uname}.chomp, explanation: "Command in PATH but not executed "},
-  {tc: 'Check how PATH lookup is implemented for a command that does not exist', depends: :EXTERNAL, cmd: 'xyz', expected: 'No such file or directory', explanation: "Command not in PATH but expected error not thrown"},
-  {tc: 'Check if relative path commands will run', depends: :EXTERNAL, cmd: relative_path_to(path_for('uname')), expected: %x{uname}.chomp, explanation: "Relative path command not executed "},
-  {tc: 'Check if environment variables are implemented', depends: :USERVARS, cmd: 'PS1="% "', expected: '% ', explanation: "Failed to set a new command prompt into PS1"},
-  {tc: 'Print environment variables with an internal command', depends: :EXTERNAL, cmd: 'env', expected: 'PS1=', explanation: "Expected to be able view environment variables with the 'env' command"},
-  {tc: 'Check if output redirection is implemented', depends: :EXTERNAL, cmd: 'echo redirection > outfile.txt', expected: 'Runnable.redirectto', explanation: "Output redirection not implemented or not working"},
-  {tc: 'Check if input redirection is implemented', depends: :EXTERNAL, cmd: 'cat < outfile.txt', expected: 'redirection', explanation: "Input redirection not implemented or not working"},
-  {tc: 'Check if command pipelining is implemented (no command arguments)', depends: :EXTERNAL, cmd: 'uname | wc' , expected: %x{uname | wc}.chomp, explanation: "Command pipelining not implemented or not working"},
-  {tc: 'Check if command pipelining is implemented (with command arguments)', depends: :EXTERNAL, cmd: 'cat outfile.txt | wc -l' , expected: '1', explanation: "Command pipelining not implemented or not working with command arguments"},
-  {tc: 'Check if command backgrounding is implemented', depends: :EXTERNAL, cmd: 'tests/sleep.sh &' , expected: @prompt, explanation: "Command backgrounding not implemented or not working"},
-  {tc: 'BONUS (Not graded): Check if simultaneuous input and output redirection is implemented', depends: :EXTERNAL, cmd: 'cat < outfile.txt > /dev/tty' , expected: 'redirection', explanation: "Simultaneous use of < and > not implemented or not working"},
-  {tc: 'BONUS (Not graded): Check if multi-stage pipelining is implemented', depends: :EXTERNAL, cmd: 'uname -a | cat | wc -l' , expected: %{uname -a | cat | wc -l}.chomp, explanation: "3-stage pipeline not implemented or not working"},
-  {tc: 'BONUS (Not graded): Check if internal commands can be pipelined', depends: :EXTERNAL, cmd: 'env|grep PATH' , expected: '/usr/bin', explanation: "3-stage pipeline not implemented or not working"},
-  {tc: 'BONUS (Not graded): Check if all redirection features work together', depends: :EXTERNAL, cmd: 'cat < outfile.txt | cat > /dev/tty ' , expected: 'redirection', explanation: "Multifuncitonal redirection not implemented or not working"},
+  {tc: 'Run a command with no arguments', depends: :EXTERNAL, cmd: path_for('uname'), expected: %x{uname}.chomp, explanation: "Unexpected output from running the a command", marks: 0 },
+  {tc: 'Check if fork() is implemented', depends: :EXTERNAL, cmd: path_for('ps'), expected: 'lsh', explanation: "Command not executed in fork()", marks: 0 },
+  {tc: 'Run a command with a single argument', depends: :EXTERNAL, cmd: path_for('uname') + ' -n', expected: %x{uname -n}.chomp, explanation: "No support for passing command line arguments (1 arg)", marks: 0 },
+  {tc: 'Run a command with three arguments', depends: :EXTERNAL, cmd: path_for('expr') + ' 1 + 1', expected: '^2', explanation: "No support for passing command line arguments (3 args)", marks: 0 },
+  {tc: 'Run a commmand with multiple arguments', depends: :EXTERNAL, cmd: path_for('expr') + ' 5 * 6 + 7 / 2 - 8 * 3', expected: '^9', explanation: "No support for passing command line arguments (11 args)", marks: 0 },
+  {tc: 'Check if PATH lookup is implemented for a command that exists', depends: :EXTERNAL, cmd: path_for('uname'), expected: %x{uname}.chomp, explanation: "Command in PATH but not executed ", marks: 0 },
+  {tc: 'Check how PATH lookup is implemented for a command that does not exist', depends: :EXTERNAL, cmd: 'xyz', expected: 'No such file or directory', explanation: "Command not in PATH but expected error not thrown", marks: 0 },
+  {tc: 'Check if relative path commands will run', depends: :EXTERNAL, cmd: relative_path_to(path_for('uname')), expected: %x{uname}.chomp, explanation: "Relative path command not executed ", marks: 0 },
+  {tc: 'Check if environment variables are implemented', depends: :USERVARS, cmd: 'PS1="% "', expected: '% ', explanation: "Failed to set a new command prompt into PS1", marks: 0 },
+  {tc: 'Print environment variables with an internal command', depends: :EXTERNAL, cmd: 'env', expected: 'PS1=', explanation: "Expected to be able view environment variables with the 'env' command", marks: 3 },
+  {tc: 'Check if command backgrounding is implemented', depends: :EXTERNAL, cmd: 'tests/sleep.sh &' , expected: @prompt, explanation: "Command backgrounding not implemented or not working", marks: 3 },
+  {tc: 'Check if output redirection is implemented', depends: :EXTERNAL, cmd: 'echo redirection > outfile.txt', expected: 'Runnable.redirectto', explanation: "Output redirection not implemented or not working", marks: 3 },
+  {tc: 'Check if input redirection is implemented', depends: :EXTERNAL, cmd: 'cat < outfile.txt', expected: 'redirection', explanation: "Input redirection not implemented or not working", marks: 3 },
+  {tc: 'Check if command pipelining is implemented (no command arguments)', depends: :EXTERNAL, cmd: 'uname | wc' , expected: %x{uname | wc}.chomp, explanation: "Command pipelining not implemented or not working", marks: 1.5 },
+  {tc: 'Check if command pipelining is implemented (with command arguments)', depends: :EXTERNAL, cmd: 'cat outfile.txt | wc -l' , expected: '1', explanation: "Command pipelining not implemented or not working with command arguments", marks: 1.5 },
+  {tc: 'BONUS (Not graded): Check if simultaneuous input and output redirection is implemented', depends: :EXTERNAL, cmd: 'cat < outfile.txt > /dev/tty' , expected: 'redirection', explanation: "Simultaneous use of < and > not implemented or not working", marks: 0 },
+  {tc: 'BONUS (Not graded): Check if multi-stage pipelining is implemented', depends: :EXTERNAL, cmd: 'uname -a | cat | wc -l' , expected: %{uname -a | cat | wc -l}.chomp, explanation: "3-stage pipeline not implemented or not working", marks: 0 },
+  {tc: 'BONUS (Not graded): Check if internal commands can be pipelined', depends: :EXTERNAL, cmd: 'env|grep PATH' , expected: '/usr/bin', explanation: "3-stage pipeline not implemented or not working", marks: 0 },
+  {tc: 'BONUS (Not graded): Check if all redirection features work together', depends: :EXTERNAL, cmd: 'cat < outfile.txt | cat > /dev/tty ' , expected: 'redirection', explanation: "Multifuncitonal redirection not implemented or not working", marks: 0 },
 ]
 
 def valid_tests
@@ -106,6 +122,7 @@ begin
             fail test[:explanation]
           else
             @passes += 1
+            @grade += test[:marks]
           end
         end
       end
@@ -118,7 +135,14 @@ rescue => e
   end
 end
 
-$stderr.puts "\n#{@passes} out of #{valid_tests.size} tests passed of #{@run} test cases run. "
+msg "\n#{@passes} out of #{valid_tests.size} tests passed of #{@run} test cases run. "
 if @run < valid_tests.size
-  $stderr.puts "#{valid_tests.size - @run} test cases could not run. Fix failing unit-test-#{@run - 1} to proceed\n"
+  msg "#{valid_tests.size - @run} test cases could not run. Fix failing unit-test-#{@run - 1} to proceed"
 end
+if @grading
+  $stderr.print "#{@grade}\n"
+end
+
+Runnable.cleanup
+
+exit 0
